de772f09ce5a3561cfbbd01cb9ee63068a42436c
Wiktor Małyska <wikimal200310@gmail.com>
2023-08-07 18:18:12 +0200

    added borger changing color every 3 secounds

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello from Nerdbord!')
+console.log('Hello from Nerdbord!');
+var box = document.querySelector('.box');
+var colors = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'pink', 'brown', 'black', 'white'];
+var colorIndex = 0;
+
+function changeColor() {
+    box.style.borderColor = colors[colorIndex];
+    colorIndex = (colorIndex+1) % colors.length;
+}
+changeColor();
+setInterval(changeColor, 3000);
